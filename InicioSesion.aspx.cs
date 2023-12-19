9da52cfe445235db0abc3d333bb74ef05c31ed8f
simja <simja@example.com>
2023-12-18 19:32:39 -0600

    modified:   InicioSesion.aspx.cs 	new file:   db/datosprueba.sql 	new file:   db/modeloDB.mwb 	new file:   db/modeloDB.mwb.bak 	new file:   db/scriptDB.sql 	new file:   lib/MySql.Data.dll

--- InicioSesion.aspx.cs
+++ InicioSesion.aspx.cs
@@ -1,11 +1,33 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
 using System.Web.UI;
+using System.Configuration;
 
 public partial class Default : Page
 {
+    private TextBox txtUsuario;
+    private TextBox txtPassword;
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Code executed when the page is loaded
+        // check if the user is logged in
+        if (Session["user"] != null)
+        {
+            // navigate to home page
+            Response.Redirect("Default.aspx");
+        }
+
+        // check if the user is admin
+        if (Session["admin"] != null)
+        {
+            // if the user is admin, show the admin button
+            if ((bool)Session["admin"])
+            {
+                Response.Redirect("Admin.aspx");
+            }
+        }
     }
     public void btnRegistrarse_Click(object sender, EventArgs e)
     {
@@ -16,8 +38,25 @@ public partial class Default : Page
 
     public void btnIniciarSesion_Click(object sender, EventArgs e)
     {
-        // navigate to default page
-        Response.Redirect("Default.aspx");
+        // get the user and password
+        string user = txtUsuario.Text;
+        string password = txtPassword.Text;
+
+        // hash the password with md5
+        string hashedPassword = GetMd5Hash(password);
+
+        // check if the user exists in the database
+        if (userExists(user, hashedPassword))
+        {
+            // navigate to the home page
+            Response.Redirect("Home.aspx");
+        }
+        else
+        {
+            // show error message
+            lblError.Text = "Usuario o contraseña incorrectos";
+        }
+        
         
     }
 
@@ -26,4 +65,60 @@ public partial class Default : Page
         // Navigate to new register page
         Response.Redirect("Default.aspx");
     }
+
+    private bool userExists(string user, string password)
+    {
+        // connection string
+        string connectionString = "Server=localhost;Database=prueba;Uid=root;Pwd=;";
+
+        // connect to the database
+        MySqlConnection conn = new MySqlConnection(connectionString);
+        conn.Open();
+
+        // query to check if the user exists
+        string query = "SELECT * FROM usuarios WHERE usuario = '" + user + "' AND password = '" + password + "'";
+
+        // execute the query
+        MySqlCommand cmd = new MySqlCommand(query, conn);
+        MySqlDataReader reader = cmd.ExecuteReader();
+
+        // if the user exists return true
+        if (reader.Read())
+        {
+            // if the id is equal to 1, the user is the admin
+            if (reader.GetInt32(0) == 1)
+            {
+                Session["admin"] = true;
+            }
+            else
+            {
+                Session["admin"] = false;
+            }
+
+            // close the connection
+            conn.Close();
+            return true;
+        }
+        // else return false
+        return false;
+    }
+
+    private string GetMd5Hash(string input)
+    {
+        using (MD5 md5Hash = MD5.Create())
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
 }
+
+
---
+++ db/datosprueba.sql
@@ -0,0 +1,54 @@
+-- insertar los productos
+-- Ejemplo 1
+INSERT INTO `ecommerce`.`Productos` (`nombre`, `desc`, `precio`) 
+VALUES ('Laptop HP Pavilion', 'Portátil potente con procesador Intel Core i5, 8 GB de RAM y SSD de 256 GB', 799.99);
+
+-- Ejemplo 2
+INSERT INTO `ecommerce`.`Productos` (`nombre`, `desc`, `precio`) 
+VALUES ('PC de Escritorio Dell Inspiron', 'Computadora de escritorio con pantalla de 23 pulgadas, procesador AMD Ryzen 7, 16 GB de RAM y HDD de 1 TB', 1299.99);
+
+-- Ejemplo 3
+INSERT INTO `ecommerce`.`Productos` (`nombre`, `desc`, `precio`) 
+VALUES ('Chromebook Acer', 'Ligero y portátil con pantalla HD de 14 pulgadas, procesador Intel Celeron, 4 GB de RAM y almacenamiento eMMC de 64 GB', 349.99);
+
+-- insertar los usuarios
+-- admin 
+INSERT INTO `ecommerce`.`Usuarios` (`correo`, `password`, `nombre`, `apellido_p`, `apellido_m`)
+VALUES ('admin@example.com', MD5('admin123'), 'Ilse', 'Olivera', 'Bruno');
+
+-- Ejemplo 1
+INSERT INTO `ecommerce`.`Usuarios` (`correo`, `password`, `nombre`, `apellido_p`, `apellido_m`)
+VALUES ('juan@example.com', MD5('juan123'), 'Juan', 'Pérez', 'Gómez');
+
+-- Ejemplo 2
+INSERT INTO `ecommerce`.`Usuarios` (`correo`, `password`, `nombre`, `apellido_p`, `apellido_m`)
+VALUES ('ana@example.com', MD5('ana456'), 'Ana', 'García', 'López');
+
+-- Ejemplo 3
+INSERT INTO `ecommerce`.`Usuarios` (`correo`, `password`, `nombre`, `apellido_p`, `apellido_m`)
+VALUES ('carlos@example.com', MD5('carlos789'), 'Carlos', 'Martínez', 'Ruiz');
+
+-- insertar las relaciones
+-- Ejemplo 1: Asociar un producto a un usuario
+INSERT INTO `ecommerce`.`Usuario_Productos` (`UP_U_Id`, `UP_P_Id`)
+VALUES (2, 2);
+
+-- Ejemplo 2: 
+INSERT INTO `ecommerce`.`Usuario_Productos` (`UP_U_Id`, `UP_P_Id`)
+VALUES (2, 2);
+
+-- Ejemplo 3: 
+INSERT INTO `ecommerce`.`Usuario_Productos` (`UP_U_Id`, `UP_P_Id`)
+VALUES (2, 3);
+
+-- Ejemplo 4: 
+INSERT INTO `ecommerce`.`Usuario_Productos` (`UP_U_Id`, `UP_P_Id`)
+VALUES (3, 2);
+
+-- Ejemplo 5: 
+INSERT INTO `ecommerce`.`Usuario_Productos` (`UP_U_Id`, `UP_P_Id`)
+VALUES (3, 3);
+
+-- Ejemplo 6: 
+INSERT INTO `ecommerce`.`Usuario_Productos` (`UP_U_Id`, `UP_P_Id`)
+VALUES (4, 3);
