fd736c9cacf89325b44896cddbfdb3538d69a6a1
simja <simja@example.com>
2023-12-20 11:24:54 -0600

    Changes to be committed: Restauracion de Default y el inicio de sesion 	modified:   Default.aspx 	modified:   Default.aspx.cs 	modified:   InicioSesion.aspx 	modified:   InicioSesion.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,17 +1,67 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
 using System.Web.UI;
+using System.Configuration;
+using System.Web.UI.WebControls;
 
 public partial class Default : Page
 {
+    public Label lblNombreUsuario;
+    public LinkButton lnkCerrarSesion;
+    public LinkButton lnkCarrito;
+    public LinkButton lnkIniciarSesion;
+    public Repeater rptProductos;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Code executed when the page is loaded
-    }
-    public void btnProductos_Click(object sender, EventArgs e)
-    {
-        // navigate to default page
-        Response.Redirect("Default.aspx");
-        
+
+
+        // check if the user is logged in
+        if (Session["user"] != null)
+        {
+            // show the user name
+            lblNombreUsuario.Text = Session["userName"].ToString() + " " + Session["userLastName"].ToString();
+
+            // show the close session button
+            lnkCerrarSesion.Visible = true;
+
+            // show the carrito button
+            lnkCarrito.Visible = true;
+
+            // hide the iniciar sesion button
+            lnkIniciarSesion.Visible = false;
+        }
+        else
+        {
+            // hide the close session button
+            lnkCerrarSesion.Visible = false;
+
+            // hide the carrito button
+            lnkCarrito.Visible = false;
+
+            // show the iniciar sesion button
+            lnkIniciarSesion.Visible = true;
+        }
+
+        // check if the user is admin
+        if (Session["admin"] != null)
+        {
+            // if the user is admin, show the admin button
+            if ((bool)Session["admin"])
+            {
+                Response.Redirect("Admin.aspx");
+            }
+        }
+
+        // get all the products
+        if (!IsPostBack)
+        {
+            getAllProducts();
+        }
+
     }
 
     public void btnIniciarSecion_Click(object sender, EventArgs e)
@@ -20,15 +70,64 @@ public partial class Default : Page
         Response.Redirect("InicioSesion.aspx");
     }
 
-    public void btnClientes_Click(object sender, EventArgs e)
-    {
-        // Navigate to search page
-        Response.Redirect("Clientes.aspx");
-    }
-
     public void btnCarrito_Click(object sender, EventArgs e)
     {
         // Navigate to search page
         Response.Redirect("Carrito.aspx");
+    }
+
+
+    public void btnCerrarSesion_Click(object sender, EventArgs e)
+    {
+        // close the session
+        Session.Remove("user");
+        Session.Remove("admin");
+
+        // navigate to default page
+        Response.Redirect("Default.aspx");
+
+    }
+
+    public void getAllProducts()
+    {
+        // get the connection string
+        string connStr = "Server=localhost;Database=ecommerce;Uid=root;Pwd=;";
+
+        // create a new connection
+        MySqlConnection conn = new MySqlConnection(connStr);
+        conn.Open();
+
+        // query to get all the products
+        string query = "SELECT * FROM productos";
+
+        // create a new data adapter based on the specified query
+        MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+
+        // create a new data table
+        DataTable dt = new DataTable();
+
+        // fill the data table and dispose the data adapter
+        adapter.Fill(dt);
+
+        // close the connection
+        conn.Close();
+
+        // set the data source of the repeater
+        rptProductos.DataSource = dt;
+
+        // bind the data
+        rptProductos.DataBind();
+
+
+    }
+
+    public void ObtenerDetalles_Click(object sender, EventArgs e)
+    {
+        // get the id of the product
+        string id = ((Button)sender).CommandArgument;
+
+        // show an alert with the id like a javascript alert
+        Response.Write("<script>alert('" + id + "')</script>");
+        
     }
 }
--- InicioSesion.aspx
+++ InicioSesion.aspx
@@ -15,12 +15,19 @@
     <div class="container">
         <div class="login">
             <h1>Inicio de Sesion</h1>
-                <asp:Label ID="lblUsuario" runat="server" Text="Usuario"></asp:Label>
-                <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
-                <asp:Label ID="lblContrasena" runat="server" Text="Contrasena"></asp:Label>
-                <asp:TextBox ID="txtContrasena" runat="server" TextMode="Password"></asp:TextBox>
+
+                <!-- label for the success message -->
+                <asp:Label ID="lblSuccess" runat="server" Text=""></asp:Label>
+
+                <asp:Label ID="lblEmail" runat="server" Text="Email"></asp:Label>
+                <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+
+                <asp:Label ID="lblPassword" runat="server" Text="Contrasena"></asp:Label>
+                <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+
                 <asp:Button ID="btnIniciarSesion" runat="server" Text="Iniciar Sesion" OnClick="btnIniciarSesion_Click" />
 
+                <!-- label for the error message -->
                 <asp:Label ID="lblError" runat="server" Text=""></asp:Label>
         </div>
     </div>
